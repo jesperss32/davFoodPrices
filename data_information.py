2f5b6df8908ed2651066d09ad33bb12d1c51dfbf
Jesper van Duuren <j.o.van.duuren@icloud.com>
2018-06-14 13:38:06 +0200

    has the ability to create a standarddiviation bar plot

--- data_information.py
+++ data_information.py
@@ -6,43 +6,33 @@ import matplotlib.pyplot as plt
 ###############################
 ##	Import dataset
 ###############################
-# TODO: this is the raw datset, in the future the preprocessed dataset should be loaded
 def loadingdata():
-	df = pd.read_csv('WFPVAM_FoodPrices_05-12-2017.csv', encoding='latin-1')
-
-	df.rename(columns={'adm0_id': 'country_ID', 'adm0_name': 'country', 'adm1_id' : 'district_ID', \
-                   'adm1_name' : 'district', 'mkt_id' : 'market_ID', 'mkt_name' : 'market' , \
-                   'cm_id' : 'product_ID','cm_name' : '_product', 'cur_id' : 'currency_ID', \
-                   'cur_name' : 'currency', 'pt_id' : 'sale_ID', 'pt_name' : 'sale', 'um_id' : 'unit_ID', \
-                   'um_name' : 'unit', 'mp_month' : 'month', 'mp_year' : 'year', 'mp_price' : 'price', \
-                   'mp_commoditysource' : 'source'}, inplace=True)
-	#df = df.head(1000)
+	df = pd.read_csv('firstclean_foodprices_data.csv', encoding='latin-1')
+	#df = df.head(10000)
 	return df
-
 
 # This find the mean of every combination of product with country.
 # This data is stored first in a numpy array, and this numpy array is loaded
 # into a pandas dataframe.
 def calculateinformation(countries, products, meanDataArray):
 	for country in countries:
-		for product in products:
-			query = ('country == "{}" & _product == "{}"' .format(country, product))
-			mean = eda.calcMean(df, query)
-			print("check")
-			if not np.isnan(mean):
-				print("test")
-				meanDataArray = np.append(meanDataArray, [[country, product, mean]], axis=0)
-				print("found a mean, generation the price history plot now...")
-				eda.boxPlot(df, query)
-	#plt.show()
-	return
+		#for product in products:
+		product = "Bread"
+		query = ('country == "{}" & _product == "{}"' .format(country, product))
+		mean = eda.calcMean(df, query)
+		if not np.isnan(mean):
+			standardDeviation = eda.calcStandardDev(df, query)
+			meanDataArray = np.append(meanDataArray, [[country, product, mean, standardDeviation]], axis=0)
+			eda.boxPlot(df, query)
+	return meanDataArray
 
 # final shaping and presentation of the new dataframe
 def shapeAndPresent(meanDataArray):
 	meandf = pd.DataFrame(meanDataArray)
 	meandf = meandf.drop(df.index[[0]])
-	print(meandf)
-	return
+	meandf.rename(columns={0: "country", 1: "product", \
+		2: "mean", 3:"standard deviation"}, inplace = True)
+	return meandf
 
 ###############################
 ##	DataFrame
@@ -59,13 +49,21 @@ countries = list(set(df['country'].tolist()))
 #creat a list of all products of the dataset
 products = list(set(df['_product'].tolist()))
 
-meanDataArray = np.array([["", "", 0.11]])
+#create array for addition data
+meanDataArray = np.array([["", "", 0.11, 0.11]])
 
-print("test")
-calculateinformation(countries, products, meanDataArray)
-shapeAndPresent(meanDataArray)
+# main funtion
+meanDataArray = calculateinformation(countries, products, meanDataArray)
+metadf = shapeAndPresent(meanDataArray)
+print(metadf)
 
+print((metadf['standard deviation'].values.tolist()))
 
+#plt.bar(len(metadf.index), metadf['standard deviation'])
+
+plt.bar(metadf['country'], metadf['standard deviation'].astype(float))
+plt.xticks(metadf['country'], rotation='vertical')
+plt.show()
 
 
 
