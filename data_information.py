3b59721058bc5c813becca074f743b297f115497
Jesper van Duuren <j.o.van.duuren@icloud.com>
2018-06-14 11:17:28 +0200

    Created ways to save plots

--- data_information.py
+++ data_information.py
@@ -1,21 +1,53 @@
 import eda
 import pandas as pd 
-import numpy as np 
+import numpy as np
+import matplotlib.pyplot as plt
 
 ###############################
 ##	Import dataset
 ###############################
 # TODO: this is the raw datset, in the future the preprocessed dataset should be loaded
+def loadingdata():
+	df = pd.read_csv('WFPVAM_FoodPrices_05-12-2017.csv', encoding='latin-1')
 
-df = pd.read_csv('data/WFPVAM_FoodPrices_05-12-2017.csv', encoding='latin-1')
-
-df.rename(columns={'adm0_id': 'country_ID', 'adm0_name': 'country', 'adm1_id' : 'district_ID', \
+	df.rename(columns={'adm0_id': 'country_ID', 'adm0_name': 'country', 'adm1_id' : 'district_ID', \
                    'adm1_name' : 'district', 'mkt_id' : 'market_ID', 'mkt_name' : 'market' , \
                    'cm_id' : 'product_ID','cm_name' : '_product', 'cur_id' : 'currency_ID', \
                    'cur_name' : 'currency', 'pt_id' : 'sale_ID', 'pt_name' : 'sale', 'um_id' : 'unit_ID', \
                    'um_name' : 'unit', 'mp_month' : 'month', 'mp_year' : 'year', 'mp_price' : 'price', \
                    'mp_commoditysource' : 'source'}, inplace=True)
+	#df = df.head(1000)
+	return df
 
+
+# This find the mean of every combination of product with country.
+# This data is stored first in a numpy array, and this numpy array is loaded
+# into a pandas dataframe.
+def calculateinformation(countries, products, meanDataArray):
+	for country in countries:
+		for product in products:
+			query = ('country == "{}" & _product == "{}"' .format(country, product))
+			mean = eda.calcMean(df, query)
+			print("check")
+			if not np.isnan(mean):
+				print("test")
+				meanDataArray = np.append(meanDataArray, [[country, product, mean]], axis=0)
+				print("found a mean, generation the price history plot now...")
+				eda.boxPlot(df, query)
+	#plt.show()
+	return
+
+# final shaping and presentation of the new dataframe
+def shapeAndPresent(meanDataArray):
+	meandf = pd.DataFrame(meanDataArray)
+	meandf = meandf.drop(df.index[[0]])
+	print(meandf)
+	return
+
+###############################
+##	DataFrame
+###############################
+df = loadingdata()
 
 ###############################
 ##	Variables
@@ -29,18 +61,12 @@ products = list(set(df['_product'].tolist()))
 
 meanDataArray = np.array([["", "", 0.11]])
 
+print("test")
+calculateinformation(countries, products, meanDataArray)
+shapeAndPresent(meanDataArray)
 
-# This find the mean of every combination of product with country.
-# This data is stored first in a numpy array, and this numpy array is loaded
-# into a pandas dataframe.
-for country in countries:
-	for product in products:
-		query = ('country == "{}" & _product == "{}"' .format(country, product))
-		mean = eda.calcMean(df, query)
-		if not np.isnan(mean):
-			meanDataArray = np.append(meanDataArray, [[country, product, mean]], axis=0)
 
-# final shaping and presentation of the new dataframe
-meandf = pd.DataFrame(meanDataArray)
-meandf = meandf.drop(df.index[[0]])
-print(meandf)
+
+
+
+
