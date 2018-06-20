2172e6d448627ad2fd293c8d32f9991c194acf37
Tessel Wisman <tesselwisman@gmail.com>
2018-06-20 13:20:55 +0200

    x

--- machine_learning.py
+++ machine_learning.py
@@ -20,8 +20,9 @@ def year_train_test_split(data, ratio):
 
 def monthly_predictions(df, country, product_ID):
     relevant_data = df.loc[(df['country'] == country) & (df['product_ID'] == product_ID)]
-    trainset, testset = year_train_test_split(relevant_data, 0.7)
 
+    trainset, testset = year_train_test_split(relevant_data, 0.8)
+    print(trainset.year.unique(), testset.year.unique())
     train_labels = np.array(trainset['price']).reshape(-1,1)
     test_labels = np.array(testset['price']).reshape(-1,1)
     train_data = np.array(trainset['month']).reshape(-1,1)
@@ -29,13 +30,12 @@ def monthly_predictions(df, country, product_ID):
 
     regr = linear_model.LinearRegression()
 
-    #
-    # poly = PolynomialFeatures(2)
-    # transform = poly.fit_transform(train_data)
-    print(np.shape(train_data), np.shape(train_labels))
 
-    regr.fit(train_data, train_labels)
-    predict = regr.predict(test_data)
+    poly = PolynomialFeatures(4)
+    transform_train = poly.fit_transform(train_data)
+    transform_test = poly.fit_transform(test_data)
+    regr.fit(transform_train, train_labels)
+    predict = regr.predict(transform_test)
     print('Coefficients: \n', regr.coef_)
     # The mean squared error
     print("Mean squared error: %.2f"
@@ -45,21 +45,24 @@ def monthly_predictions(df, country, product_ID):
 
     # Plot outputs
     plt.scatter(test_data, test_labels,  color='black')
-    plt.plot(test_data, predict, color='blue', linewidth=3)
+    plt.plot(test_data, predict, color='blue', linewidth=2)
     plt.xlabel('month')
     plt.ylabel('price')
-    plt.xticks(())
-    plt.yticks(())
+    # plt.xticks(())
+    # plt.yticks(())
 
     plt.show()
 
 
 
 
-
 if __name__ == "__main__":
-    data = pd.read_csv('secondclean_foodprices_data.csv')
-    av = monthly_predictions(data, 'India', 114)
+    data = pd.read_csv('fooddatasets/only_complete_years_data.csv')
+    # print(data.loc[data['country'] == 'Haiti'].year.unique())
+    # print(data.loc[data['country'] == 'Haiti']._product.unique())
+    # for p in data.loc[data['country'] == 'Haiti']._product.unique():
+    #     print(list(set(data.loc[(data['country'] == 'Haiti') & (data['_product'] == p)].product_ID)))
+    av = monthly_predictions(data, 'Haiti', 339)
     # 84 = wheat
     # 145 = rice low q
     # 114 = tomatoes
